7300f567dfc1df52b4934461f80bffa04dd2f7d8
chananate <chananate.k@gmail.com>
2019-06-13 16:17:37 +0700

    playplay

--- src/app/modules/main/main-page/main-page.component.ts
+++ src/app/modules/main/main-page/main-page.component.ts
@@ -1,4 +1,6 @@
+import { PatientService } from './../../../services/patient.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  userList: any[] = [];
+  
+  constructor(
+    private userService: PatientService
+  ) { }
 
   ngOnInit() {
+    this.getUsers();
   }
+
+  async getUsers(){
+    const result : any = await this.userService.getUser();
+    if(result.statusCode===200 && result.rows.length){
+      this.userList=result.rows;
+      console.log(this.userList);
+    }
+  }
+
 
 }
--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -10,4 +10,13 @@ export class PatientService {
     private http: HttpClient,
     @Inject("API_URL") private apiUrl
   ) { }
+
+  getUser(){
+    return this.http.get("http://localhost:3000/user-info/user-all",{})
+      .toPromise()
+      .then(result => {
+        return result;
+      })
+      .catch(err => err);
+  }
 }
