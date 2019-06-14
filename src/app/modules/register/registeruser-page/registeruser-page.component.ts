8bdd3c68f4e5850dca79977debf9c344285bce9b
nitchakarnpattala <47791591+nitchakarnpattala@users.noreply.github.com>
2019-06-14 08:56:46 +0700

    RegisAndPatientExp

--- src/app/modules/register/registeruser-page/registeruser-page.component.ts
+++ src/app/modules/register/registeruser-page/registeruser-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { PatientService } from './../../../services/patient.service';
+import { Component, OnInit } from "@angular/core";
+import { AlertService } from 'src/app/services/alert.service';
 
 @Component({
   selector: 'app-registeruser-page',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registeruser-page.component.scss']
 })
 export class RegisterUserPageComponent implements OnInit {
+  patientList: any[] = [];
+  prefixList: any[];
+  modalEdit = false;
+  patient_personalId :number;
+  patient_title:string;
+  patient_name:string;
+  patient_surname:string;
+  patient_bd:Date;
+  patient_address:string;
+  patient_religion:string;
 
-  constructor() { }
+  constructor(private patientService: PatientService,
+    private alert: AlertService
+    ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.getPatient();
+  }
+  
+  async getPatient() {
+    const result: any = await this.patientService.getPatient();
+    if (result.statusCode === 200 && result.rows.length) {
+      this.patientList = result.rows;
+      console.log(this.patientList);
+    }
   }
 
+  async onSave() {
+    let result: any;
+
+      result = await this.patientService
+      .insertPatient(this.patient_personalId,this.patient_title,
+        this.patient_name,
+        this.patient_surname,
+        this.patient_bd,
+        this.patient_address,
+        this.patient_religion);
+      console.log('insert', result);
+      await this.getPatient();
+
+  }
 }
+
--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -11,12 +11,31 @@ export class PatientService {
     @Inject("API_URL") private apiUrl
   ) { }
 
-  getUser(){
-    return this.http.get("http://localhost:3000/user-info/user-all",{})
+  getPatient(){
+    return this.http.get("http://localhost:3000/pat-info/user-all",{})
       .toPromise()
       .then(result => {
         return result;
       })
       .catch(err => err);
+  }
+
+  getPatientInfo(Patient_personalId){
+    return this.http.post("http://localhost:3000/pat-info/employee-info",{Patient_personalId})
+    .toPromise()
+    .then(result => result)
+    .catch(error=>error);
+  }
+
+  insertPatient(Patient_personalId
+    ,Patient_title,Patient_name,Patient_surname
+    ,Patient_bd,Patient_address,Patient_religion){
+    return this.http.post("http://localhost:3000/pat-info/patient-insert"
+    ,{Patient_personalId
+      ,Patient_title,Patient_name,Patient_surname
+      ,Patient_bd,Patient_address,Patient_religion})
+    .toPromise()
+    .then(result => result)
+    .catch(error=>error);
   }
 }
