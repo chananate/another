d5b7bce621fca6cb94da738f89fa54584a7a8174
chananate <chananate.k@gmail.com>
2019-06-28 09:39:13 +0700

    for you amrtass

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -8,17 +8,18 @@ export class EmployeeService {
 
   constructor(
     private http: HttpClient,
-    @Inject("API_URL") private apiUrl
+    @Inject('API_URL') private apiUrl
   ) { }
-  getEmployeeAll(){
-    return this.http.get("http://localhost:3000/emp-info/employee-all",{})
+
+  getEmployeeAll() {
+    return this.http.get('http://localhost:3000/emp-info/employee-all',{})
     .toPromise()
     .then(result => result)
-    .catch(error=>error);
+    .catch(error => error);
   }
 
   getEmployeeInfo(employee_personalId){
-    return this.http.post("http://localhost:3000/emp-info/employee-info",{employee_personalId})
+    return this.http.post('http://localhost:3000/emp-info/employee-info',{employee_personalId})
     .toPromise()
     .then(result => result)
     .catch(error=>error);
@@ -27,7 +28,7 @@ export class EmployeeService {
   insertEmp(employee_personalId,employee_position,employee_type
     ,employee_title,employee_name,employee_surname
     ,employee_bd,employee_address,employee_religion,employee_tel){
-    return this.http.post("http://localhost:3000/emp-info/employee-insert"
+    return this.http.post('http://localhost:3000/emp-info/employee-insert'
     ,{employee_personalId,employee_position
       ,employee_type,employee_title,employee_name,employee_surname
       ,employee_bd,employee_address,employee_religion,employee_tel})
@@ -37,14 +38,14 @@ export class EmployeeService {
   }
 
   updateEmployee(employee_personalId){
-    return this.http.post("http://localhost:3000/emp-info/employee-update",{employee_personalId})
+    return this.http.post('http://localhost:3000/emp-info/employee-update',{employee_personalId})
     .toPromise()
     .then(result => result)
     .catch(error=>error);
   }
   
   delEmployee(employee_personalId){
-    return this.http.post("http://localhost:3000/emp-info/employee-del",{employee_personalId})
+    return this.http.post('http://localhost:3000/emp-info/employee-del',{employee_personalId})
     .toPromise()
     .then(result => result)
     .catch(error=>error);
